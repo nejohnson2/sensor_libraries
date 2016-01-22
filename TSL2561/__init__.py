9b8785de05b7832eceafc1cbda59f09dd825e406
Nicholas Johnson <nejohnson2@gmail.com>
2016-01-22 11:09:09 -0500

    initial sensor code

--- TSL2561/__init__.py
+++ TSL2561/__init__.py
@@ -1,2 +1,1 @@
 from TSL2561 import *
-#from Adafruit_I2C import *
--- urban_monitor.py
+++ urban_monitor.py
@@ -1,3 +1,28 @@
 from BME280 import BME280
 from ShinyeiPPD42 import Shinyei
 from TSL2561 import TSL2561
+
+def main():
+	sensor = BME280(mode=BME280_OSAMPLE_8)
+    tsl = TSL2561()
+	GPIO.setmode(GPIO.BCM)
+	shinyei = Shinyei(18)
+    	    
+    while True:    
+        degrees = sensor.read_temperature()
+        pascals = sensor.read_pressure()
+        hectopascals = pascals / 100
+        humidity = sensor.read_humidity()
+        lux = tsl.lux()
+        shinyei.read(30)
+
+        print 'Timestamp = {0:0.3f}'.format(sensor.t_fine)
+        print 'Temp      = {0:0.3f} deg C'.format(degrees)
+        print 'Pressure  = {0:0.2f} hPa'.format(hectopascals)
+        print 'Humidity  = {0:0.2f} %'.format(humidity)
+        print 'Lux       = {0} %}'.format(lux)
+        time.sleep(5)	
+
+if __name__ == '__main__':
+	main()
+	GPIO.cleanup()	    
