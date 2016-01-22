92e751122668b7beb8ecfa6e2001f274223fefa2
Nicholas Johnson <nejohnson2@gmail.com>
2016-01-22 16:16:03 -0500

    minor bug

--- urban_monitor.py
+++ urban_monitor.py
@@ -6,25 +6,25 @@ def main():
 	GPIO.setmode(GPIO.BCM)
 
 	sensor = BME280(mode=BME280_OSAMPLE_8)
-    tsl = TSL2561()
+	tsl = TSL2561()
 	shinyei = Shinyei(18)	# add pin number
-    	    
-    while True:    
-        shinyei.read(30)						# I believe this is a blocking module
+	    
+	while True:    
+		shinyei.read(30)						# I believe this is a blocking module
 
-        degrees = sensor.read_temperature()
-        pascals = sensor.read_pressure()
-        hectopascals = pascals / 100
-        humidity = sensor.read_humidity()
-        
-        lux = tsl.lux()
+		degrees = sensor.read_temperature()
+		pascals = sensor.read_pressure()
+		hectopascals = pascals / 100
+		humidity = sensor.read_humidity()
 
-        print 'Timestamp = {0:0.3f}'.format(sensor.t_fine)
-        print 'Temp      = {0:0.3f} deg C'.format(degrees)
-        print 'Pressure  = {0:0.2f} hPa'.format(hectopascals)
-        print 'Humidity  = {0:0.2f} %'.format(humidity)
-        print 'Lux       = {0} %}'.format(lux)
-        #print 'Particles per 0.01 cubic feet:'
+		lux = tsl.lux()
+
+		print 'Timestamp = {0:0.3f}'.format(sensor.t_fine)
+		print 'Temp      = {0:0.3f} deg C'.format(degrees)
+		print 'Pressure  = {0:0.2f} hPa'.format(hectopascals)
+		print 'Humidity  = {0:0.2f} %'.format(humidity)
+		print 'Lux       = {0} %}'.format(lux)
+		#print 'Particles per 0.01 cubic feet:'
 
 if __name__ == '__main__':
 	main()
