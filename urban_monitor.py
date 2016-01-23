0a55e0770aee658d79859df6a7520f0ee84af1bb
Nicholas Johnson <nejohnson2@gmail.com>
2016-01-22 20:41:52 -0500

    added csv writer

--- urban_monitor.py
+++ urban_monitor.py
@@ -1,31 +1,47 @@
+import csv, os.path
 import RPi.GPIO as GPIO
 from BME280 import BME280
 from ShinyeiPPD42 import Shinyei
 from TSL2561 import TSL2561
 
+def write_csv(data):
+	fname = './data.csv'
+
+	with open(fname, 'a') as f:
+		w = csv.DictWriter(f, data.keys())
+		w.writerow(data)
+
+	f.close()
+
 def main():
 	GPIO.setmode(GPIO.BCM)
-
-	sensor = BME280()
-	tsl = TSL2561()
-	shinyei = Shinyei(18)	# add pin number
+	
+	bme = BME280()			# initialize bme280
+	tsl = TSL2561()			# initialize tsl2561
+	shinyei = Shinyei(18)	# initialize shinyei on gpio pin
 	    
 	while True:    
-		shinyei.read(30)						# I believe this is a blocking module
+		aq = shinyei.read(30)				# I believe this is a blocking module
 
-		degrees = sensor.read_temperature()
-		pascals = sensor.read_pressure()
+		degrees = bme.read_temperature()
+		pascals = bme.read_pressure()
 		hectopascals = pascals / 100
-		humidity = sensor.read_humidity()
-
+		humidity = bme.read_humidity()
 		lux = tsl.lux()
 
-		print 'Timestamp = {0:0.3f}'.format(sensor.t_fine)
-		print 'Temp      = {0:0.3f} deg C'.format(degrees)
-		print 'Pressure  = {0:0.2f} hPa'.format(hectopascals)
-		print 'Humidity  = {0:0.2f} %'.format(humidity)
-		print 'Lux       = {0} %}'.format(lux)
-		#print 'Particles per 0.01 cubic feet:'
+		data = {
+			'Time' : time.time(),
+			'Temp' : bme.read_pressure(),
+			'Pressure' : bme.read_pressure() / 100,
+			'Humidity' : bme.read_humidity(),
+			'Lux' : tsl.lux(),
+			'LPO' : aq[0],
+			'Ratio' : aq[1],
+			'Concentration' : aq[2],
+		}
+
+		write_csv(data)
+		print data
 
 if __name__ == '__main__':
 	main()
